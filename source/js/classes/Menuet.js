5ff4ce46870c1c9ab04ec0cb1e80162e254dfa57
Ralph Samaniego <rapsamaniego27@gmail.com>
2021-05-29 11:23:19 +0800

    added menuet functioning

--- source/js/classes/Menuet.js
+++ source/js/classes/Menuet.js
@@ -1,27 +1,3 @@
-class Data{
- constructor(){
-  
- }
-
- //Methods
- async fetchSiblings(){
-    let api = '../data/siblings.json';
-    let response = await fetch(api);
-    let data = await response.json();
- 
-    return data;
-  }
-}
-
-/* Run this for testing API fetching */
-
-/* const data = new Data();
-
-data.fetchSiblings()
-    .then(data => {
-     console.log(data);
-    })
-    .catch(err => console.log(err)); */
 /* Notes:
   <a> should always be the one clickable having a full height and width
 
@@ -148,46 +124,3 @@ class Menuet {
   }
 
 }
-console.log('sample file from Classes folder');
-
-console.log('component file from components folder');
-
-let pattern;
-
-/* Regular expression pattern */
-pattern = /hello/i;
-
-//String to match
-const str = 'Hello';
-
-//Log Results
-const result = pattern.exec(str);
-console.log(result);
-
-
-/* Function to test if the str passes the pattern. */
-function patternTest(pattern, str){
- if(pattern.test(str)){
-  console.log(`${str} matches ${pattern.source}`);
- }else{
-  console.log(`${str} does NOT match ${pattern.source}`);
- }
-}
-
-patternTest(pattern, str);
-const navOpen = document.querySelector('#menuOpen');
-const navOverlay = document.querySelector('#cmodalOverlay');
-const nav = document.querySelector('#cmenu');
-const navClose = document.querySelector('#menuClose');
-
-//Instantiate
-const menuet = new Menuet({
-  nav: nav,
-  openTrigger: navOpen,
-  closeTrigger: navClose,
-  overlay: navOverlay,
-  subMenus: ''
-});
-
-//Display output
-
--- source/js/misc/nav.js
+++ source/js/misc/nav.js
@@ -1,1 +1,15 @@
-console.log('Nav file from misc folder');
+const navOpen = document.querySelector('#menuOpen');
+const navOverlay = document.querySelector('#cmodalOverlay');
+const nav = document.querySelector('#cmenu');
+const navClose = document.querySelector('#menuClose');
+
+//Instantiate
+const menuet = new Menuet({
+  nav: nav,
+  openTrigger: navOpen,
+  closeTrigger: navClose,
+  overlay: navOverlay,
+  subMenus: ''
+});
+
+//Display output
